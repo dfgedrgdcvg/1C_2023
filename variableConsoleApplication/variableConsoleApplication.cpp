da392b9f62803367c3b7c1e88dee0e6fba41a637
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-25 16:00:19 +0200

    koniec lekcji

---
+++ variableConsoleApplication/variableConsoleApplication.cpp
@@ -0,0 +1,15 @@
+// variableConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+/*
+algorytm- skonczony zbior instrukcji, ktory rozwiazuje zadany problem 
+okresla tez kolejnosc wykonywaniaa instrukcji.
+zapis algorytmu
+*/
