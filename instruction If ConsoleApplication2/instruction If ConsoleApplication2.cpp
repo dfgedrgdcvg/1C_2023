20dcc9694bd8991c48d3e67eda72827811472808
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-17 09:49:43 +0200

    werwefrwefrwe

--- instruction If ConsoleApplication2/instruction If ConsoleApplication2.cpp
+++ instruction If ConsoleApplication2/instruction If ConsoleApplication2.cpp
@@ -8,6 +8,20 @@ operatory warunkowe:
 <= - mniejsze lub równe 
 = = - rowne
 != - ró¿ne
+
+operatory logiczne
+&& - AND
+|| - OR
+! - NOT
+                                    ALBO 
+a    b    a&&b    a||b    !a       a XOR b
+F    F     F        F      T          F
+F    T     F        T      T          T 
+T    F     F        T      F          T
+T    T     T        T      F          F
+
+F - False
+T - True
 */
 
 //napisz program ktory wyswietli informacje czy liczba jest dodatnia czy nie.
@@ -40,11 +54,108 @@ void task1()
 	if (rest == 0)
 		std::cout << "liczba jest parzysta\n";
 		if (rest != 0) //rest ==1
-			std::cout << "liczba jest nieparzysta\n";
+		std::cout << "liczba jest nieparzysta\n";
 }
+
+//napisz program ktory wyswietli informacje czy liczba jest z zakresu <1 ; 10)
+void task3()
+{
+	int number;
+	std::cout << "Podaj liczbe : \n";
+	std::cin >> number;
+	
+	//wersja 1
+	if (number >= 1)  //number <= 9
+	{
+		if (number < 10)
+			std::cout << "liczba jest w przedziale\n";
+		else
+			std::cout << "liczba jest z poza zakresu\n";
+	}
+	else
+		std::cout << "liczba z poza zakresu\n";
+
+	//wersja 2
+	if (number >= 1 && number >= 10)
+		std::cout << "liczba jest w przedziale\n";
+	else
+		std::cout << "liczba jest z poza zakresu\n";
+
+	if (number < 1 || number >= 10)
+		std::cout << "liczba z poza zakresu\n";
+	else
+		std::cout << "liczba jest w przedziale\n";
+
+	//wersja 3
+	if (!(number >= 1 && number < 10))
+		std::cout << "liczba jest z poza zakresu\n";
+	else
+		std::cout << "liczba jest w przedziale\n";
+}
+
+//napisz program ktory wykona dzielenia dwoch liczb
+void task4()
+{
+	int firstnumber, secondnumber;
+	std::cout << "podaj liczbe:\n";
+	std::cin >> firstnumber;
+	std::cout << "podaj liczbe:\n";
+	std::cin >> secondnumber;
+
+	if (secondnumber != 0)
+	{
+		int quotent = firstnumber / secondnumber;
+		std::cout << "wynik dzielenia " << quotent << "\n";
+
+	}
+	else
+		std::cout << "dzielenie przez zero!!!\n";
+}
+
+//*. Napisz program, który poprosi u¿ytkownika o podanie dwóch liczb ca³kowitych i sprawdzi, czy s¹ one równe. Wyœwietl odpowiedni komunikat.
+void task5()
+{
+	int firstnumber, secondnumber;
+	std::cout << "podaj pierwsza liczbe \n";
+	std::cin >> firstnumber;
+	std::cout << "podaj druga liczbe \n";
+	std::cin >> secondnumber;
+	if (firstnumber == secondnumber)
+	{
+		std::cout << "liczby sa równe \n";
+	}
+	else
+		std::cout << "liczby nie sa rowne! \n";
+}
+
+//*. Napisz program, który poprosi u¿ytkownika o podanie wieku i sprawdzi, czy osoba jest pe³noletnia. Wyœwietl odpowiedni komunikat.
+void task6()
+{
+	int age;
+	std::cout << "podaj wiek u¿ytkownika \n";
+	std::cin >> age;
+}
+
+
 int main()
 {
 	task1();
 }
 
 
+
+
+
+
+/*
+*. Napisz program, który poprosi u¿ytkownika o podanie liczby ca³kowitej i obliczy jej wartoœæ bezwzglêdn¹. Wyœwietl wynik.
+*. Napisz program, który poprosi u¿ytkownika o podanie liczby od 1 do 7 i wyœwietli odpowiadaj¹cy mu dzieñ tygodnia.
+*. Napisz program, który poprosi u¿ytkownika o podanie dwóch liczb ca³kowitych i wyœwietli wiêksz¹ z nich.
+*. Napisz program, który poprosi u¿ytkownika o podanie roku i sprawdzi, czy jest to rok przestêpny. Wyœwietl odpowiedni komunikat.
+*. Napisz program, który poprosi u¿ytkownika o podanie liczby ca³kowitej i sprawdzi, czy jest ona podzielna zarówno przez 3, jak i przez 5. Wyœwietl odpowiedni komunikat.
+*. Napisz program, który poprosi u¿ytkownika o podanie masy cia³a (w kilogramach) i wzrostu (w metrach).
+Na podstawie tych danych oblicz wskaŸnik BMI (Body Mass Index) i wyœwietl odpowiedni komunikat informuj¹cy o stanie zdrowia.
+*. Napisz program, który poprosi u¿ytkownika o podanie d³ugoœci trzech odcinków i sprawdzi, czy mo¿na zbudowaæ z nich trójk¹t. Wyœwietl odpowiedni komunikat.
+*. Napisz program, który poprosi u¿ytkownika o podanie liczby dodatniej i obliczy jej pierwiastek kwadratowy, jeœli jest to mo¿liwe. Wyœwietl wynik lub odpowiedni komunikat, jeœli liczba jest ujemna.
+*. Program sprawdzaj¹cy czy podana data jest poprawna (np. sprawdzaj¹c, czy dzieñ jest z zakresu od 1 do 31, miesi¹c od 1 do 12 itd.)
+*/
