38748902194798b0b78b2b123abae4bbda8b44e8
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-12-08 14:22:56 +0100

    vgsfgsdergsegsgsg

--- Programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
+++ Programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
@@ -30,11 +30,128 @@ void task2()
 
 }
 
-/* 
-* Program obliczający n!.
-* Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1=1, 1x2=2, 1x3=3 itd.).
-* Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
-* Program obliczający sumę kwadratów liczb od 1 do 10
+//Program obliczający sumę liczb od 1 do 100
+void task3()
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		sum += i;
+	}
+	std::cout << sum << "\n";
+}
+
+//Program sprawdzający czy podana liczba jest liczbą doskonałą (czyli taką,
+//  której suma dzielników (z wyłączeniem samej siebie) jest równa danej liczbie,
+//  np. 6 jest liczbą doskonałą, ponieważ 1 + 2 + 3 = 6).
+void task4()
+{
+	int number, i;
+	int sum = 0;
+	std::cout << "Podaj liczbe: ";
+	std::cin >> number;
+
+	for (i = 1; i < number; i++)
+	{
+		if (number % i == 0)
+			sum = sum + i;
+	}
+
+	if (sum == number)
+		std::cout << "Liczba jest doskonala\n";
+	else
+		std::cout << "Liczba nie jest doskonala\n";
+}
+
+//Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.)
+void task5()
+{
+	int number, i;
+
+	for (i = 1; i < 10; i++)
+	{
+		number = i * i;
+		std::cout << number << "\n";
+	}
+}
+
+//Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1 = 1, 1x2 = 2, 1x3 = 3 itd.).
+void task6()
+{
+	int number, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		std::cout << "1x" << i << " = ";
+		number = 1 * i;
+		std::cout << number << "\n";
+	}
+}
+
+//Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
+void task7()
+{
+	int number;
+	std::cout << "Liczby podzielneprzez 3 z zakresu 1 - 100:\n";
+
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			std::cout << i << "\n";
+	}
+}
+
+//Program obliczający sumę kwadratów liczb od 1 do 10
+void task8()
+{
+	int number;
+	int sum = 0;
+
+	for (int i = 1; i <= 10; i++)
+	{
+		number = i * i;
+		sum = sum + number;
+	}
+	std::cout << "Suma kwadratow liczb 1 - 10 = " << sum << "\n";
+}
+
+//Program obliczający n!  (4! = 1*2*3*4)
+void task9()
+{
+	int number;
+	int x = 0;
+	int result = 1;
+	std::cout << "Podaje liczbe: ";
+	std::cin >> number;
+
+	for (int i = 1; i <= number; i++)
+	{
+		x++;
+		result = result * x;
+	}
+	std::cout << "n! = " << result;
+}
+
+//Program wyświetlający na ekranie silnie z liczb od 1 do 10 (np. 1!, 2!, 3!, 4! itd.) 4! = 1*2*3*4
+void task10()
+{
+
+}
+
+int main()
+{
+	task10();
+}
+
+
+/*
+*
+*
+*
+*
+*
 * Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu (ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)
 
 * Program, kóry wyświetli poniższe cztery zwory:
@@ -47,81 +164,3 @@ void task2()
 
 * Rozkład liczby na czynniki pierwsze
 */
-
-//* Program obliczający sumę liczb od 1 do 100
-void task3()
-{
-	int sum = 0, i = 0;
-
-	for (i = 0; i < 100; i++)
-	{
-		sum += i;
-	}
-	std::cout << sum << "\n";
-}
-
-//*Program sprawdzający czy podana liczba jest liczbą doskonałą(czyli taką,
-//  której suma dzielników(z wyłączeniem samej siebie) jest równa danej liczbie,
-//  np. 6 jest liczbą doskonałą, ponieważ 1 + 2 + 3 = 6).
-void task4()
-{
-	int number, i;
-	int sum = 0;
-	std::cout << "podaj liczbe: ";
-	std::cin >> number;
-
-	for (i = 1; i < number; i++)
-	{
-		if (number % i == 0)
-			sum = sum + 1;
-	}
-
-	if (sum == number)
-		std::cout << "liczba jest doskonala\n";
-	else
-		std::cout << "liczba nie jest doskonala\n";
-}
-
-//*Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.)
-void task5()
-{
-	int number, i;
-
-	for (int i = 0; i < 10; i++)
-	{
-		number = i * i;
-		std::cout << number << "\n";
-	}
-}
-
-//* Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1=1, 1x2=2, 1x3=3 itd.).
-void task6()
-{
-	int number, i;
-
-	for (i = 0; i <= 9; i++)
-	{
-		std::cout << "lx" << i << " = ";
-		number = 1 * i;
-		std::cout << number << "\n";
-	}
-}
-
-//* Program wyświetlający na ekranie silnie z liczb od 1 do 10 (np. 1!, 2!, 3!, 4! itd.)
-void task7()
-{
-	int number;
-	std::cout << "liczby podzielne pzez 3 z zakresu 1 - 100:\n";
-
-	for (int i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0)
-			std::cout << i << "\n";
-	}
-}
-
-int main()
-{
-	task3();
-}
-
