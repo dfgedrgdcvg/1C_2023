46aee4d773976df4ffa9c87df4af935b2c2e6c34
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-01-11 08:07:09 +0100

    koniec zajec

--- Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
+++ Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 
 //napisz program ktory wczyta np. 5 liczb i wyswietli je w odwrotnej kolejnosci
 void task1()
@@ -17,6 +17,33 @@ void task1()
         std::cout << numbers[i] << " , ";
     }
     std::cout << "\n";
+}
+
+void task2()
+{
+	const unsigned short ARRAY_SIZE = 5;
+	int numbers[ARRAY_SIZE];
+
+	const unsigned short LOWER_RANGE = 5;
+	const unsigned short UPPER_RANGE = 7;
+
+	srand(time(NULL));
+
+	std::cout << "wylosowane liczby: \n";
+	for (int i = 0; i < ARRAY_SIZE; i++)
+	{
+		numbers[i] = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+		std::cout << numbers[i] << ", ";
+	}
+	std::cout << "\n";
+
+	int sum = 0;
+	for (int i = 0; i < ARRAY_SIZE; i++)
+	{
+		sum = sum + numbers[i];
+	}
+	double avg = sum * 1.0 / ARRAY_SIZE;
+	std::cout << "Œrednia wynosi: " << avg << "\n";
 }
 
 int main()
