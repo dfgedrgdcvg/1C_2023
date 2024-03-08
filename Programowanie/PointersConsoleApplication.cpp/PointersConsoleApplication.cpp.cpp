2cfd3e41f4643b77729a18ffd23bb6648d741eb8
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-03-08 09:55:11 +0100

    koniec zajec

--- Programowanie/PointersConsoleApplication.cpp/PointersConsoleApplication.cpp.cpp
+++ Programowanie/PointersConsoleApplication.cpp/PointersConsoleApplication.cpp.cpp
@@ -22,6 +22,22 @@ void task1()
     std::cout << "Zawartosc pierwszej komorki " << *(tab + 1) << "\n";
 }
 
+void task2()
+{
+    int firstNumber = 48;
+    int secondNumber = 697;
+
+    int* ptr = &firstNumber;
+
+    std::cout << "Adres pierwszej zmiennej \t" << &firstNumber << "\n";
+    std::cout << "Adres zmiennej \t\t\t" << ptr << "\n";
+    std::cout << "Zawartoœæ pierszej zmiennej \t" << firstNumber << "\n";
+    std::cout << "Zawartoœæ pierszej zmiennej \t" << *ptr << "\n";
+
+    ptr = &secondNumber;
+    std::cout << "Zawartoœæ drugiej zmiennej \t" << *ptr << "\n";
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
