f4754daeeb3b074d31f8cbe88e15d3bb628a4cef
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-05-22 09:02:06 +0200

    koniec zajec

---
+++ Programowanie/SquareConsoleApplication/SquareConsoleApplication.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+enum keyCode
+{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	ESC
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
