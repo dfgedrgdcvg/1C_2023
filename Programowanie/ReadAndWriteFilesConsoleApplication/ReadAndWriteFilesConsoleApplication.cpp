f27a2a643e4df0435269e7cc8708b5ae35306d4c
q <q>
2024-10-21 09:51:42 +0200

    gg

---
+++ Programowanie/ReadAndWriteFilesConsoleApplication/ReadAndWriteFilesConsoleApplication.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+
+void task1()
+{
+	std::string name;
+	int age;
+
+	std::cout << "Podaj imie";
+	std::cin >> name;
+	std::cout << "Podaj wiek";
+	std::cin >> age;
+
+	std::ofstream writeToFile;
+	writeToFile.open("c:\\plik2C.txt", std::ios_base::app);
+
+	if (writeToFile.is_open() == true)
+	{//prace na pliku
+		writeToFile << name << "\n" << age << "\n";
+		writeToFile.flush();
+		//writeToFile<<age;
+		writeToFile.close();
+
+	}
+}
+
+void task2()
+{
+	std::ifstream readFromFile;
+	readFromFile.open("c::\\plik2C.txt");
+	if (readFromFile.is_open())
+	{
+		std::string name;
+		int age;
+
+		while (readFromFile >> name)
+		{
+			readFromFile >> age;
+			//readFromFile >> name;
+
+			std::cout << "Imie" << name << "wiek" << age;
+		}
+	}
+}
+
+int main()
+{
+	task2();
+
+}
+
---
+++ Programowanie/StringConsoleApplication/StringConsoleApplication.cpp
@@ -0,0 +1,16 @@
+// StringConsoleApplication.cpp 
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+
+    std::string text;
+
+    text = "Hello";
+    std::cout << "Nasz tekst na pocz¹tku: " << text << "\n";
+    std::cout << "D³ugoœæ ³añcucha to: " << text.length() << "\n";
+    std::cout << "Czy zmienna jest pusta: " << text.empty() << "\n";
+}
+
