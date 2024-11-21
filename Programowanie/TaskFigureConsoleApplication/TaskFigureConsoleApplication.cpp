1b91d4b71eb54bec434b7e55e18066246dfd163d
q <q>
2024-11-21 09:53:57 +0100

    gg

---
+++ Programowanie/TaskFigureConsoleApplication/TaskFigureConsoleApplication.cpp
@@ -0,0 +1,11 @@
+// TaskFigureConsoleApplication.cpp 
+//Napisz klasy które opisz¹ figury geometryczne:
+//trapez, romb, równoleg³obok, prostok¹t i kwadrat.
+//Zdefiniuj podstawowe metody.
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
