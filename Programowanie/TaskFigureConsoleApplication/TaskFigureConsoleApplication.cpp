29a2ebe272b6c1927f1b23dbe8e76ec5b4baa9e1
q <q>
2024-11-25 09:48:10 +0100

    gg

--- Programowanie/TaskFigureConsoleApplication/TaskFigureConsoleApplication.cpp
+++ Programowanie/TaskFigureConsoleApplication/TaskFigureConsoleApplication.cpp
@@ -5,6 +5,37 @@
 #include <iostream>
 #include <cmath>
 
+class trapeze {
+public:
+    double a, b, h;
+
+    trapeze(double a, double b,double h) : a(a), b(b), h(h) {}
+
+    double area() {
+        return (a + b) * h / 2;
+    }
+
+    double circuit() {
+        double side = sqrt(pow((b - a) / 2, 2) + pow(h, 2));
+        return a + b + 2 * side;
+    }
+};
+
+class rhombus {
+public:
+    double a, h;
+
+    rhombus(double a, double h) : a(a), h(h) {}
+
+    double area() {
+        return a * h;
+    }
+
+    double circuit() {
+        return 4 * a;
+    }
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
