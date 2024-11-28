e4f2817cabf494e9cc4bf8c8e5ed1201ca4243dc
q <q>
2024-11-28 09:49:44 +0100

    gg

--- Programowanie/TaskFigureConsoleApplication/TaskFigureConsoleApplication.cpp
+++ Programowanie/TaskFigureConsoleApplication/TaskFigureConsoleApplication.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <cmath>
 
+//trapez
+
 class trapeze {
 public:
     double a, b, h;
@@ -21,6 +23,8 @@ public:
     }
 };
 
+//romb
+
 class rhombus {
 public:
     double a, h;
@@ -36,7 +40,67 @@ public:
     }
 };
 
+//rownoleglobok
+
+class parallelogram {
+public:
+    double a, b, h;
+    parallelogram(double a, double b, double h) : a(a), b(b), h(h) {}
+
+    double area() {
+        return a * h;
+    }
+
+    double circuit() {
+        return 2 * (a + b);
+    }
+};
+
+//prostok¹t
+
+class rectangle {
+public:
+    double a, b;
+
+    rectangle(double a, double b) : a(a), b(b) {}
+
+    double area() {
+        return a * b;
+    }
+
+    double circuit() {
+        return 2 * (a + b);
+    }
+};
+
+//kwadrat
+
+class square {
+public:
+    double a;
+
+    square(double a) : a(a) {}
+
+    double area() {
+        return a * a;
+    }
+
+    double circuit() {
+        return 4 * a;
+    }
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    trapeze trapeze(3, 4, 5);
+    rhombus rhombus(4, 3);
+    parallelogram parallelogram(3, 4, 5);
+    rectangle rectangle(4, 5);
+    square square(4);
+
+    std::cout << "pole trapeza : " << trapeze.area() << "obwod : " << trapeze.circuit() << std::endl;
+    std::cout << "pole rombu : " << rhombus.area() << "obwod : " << rhombus.circuit() << std::endl;
+    std::cout << "pole rownoegloboku : " << parallelogram.area() << "obwod : " << parallelogram.circuit() << std::endl;
+    std::cout << "pole prostokata : " << rectangle.area() << "obwod : " << rectangle.circuit() << std::endl;
+    std::cout << "pole kwadratu : " << square.area() << "obwod : " << square.circuit() << std::endl;
 }
