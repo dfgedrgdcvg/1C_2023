ab7cc028bdb0cc38a2e9b4f56e84b01ebac7093a
O <O>
2025-03-19 13:16:00 +0100

    gg

--- Programowanie/Matura2022MajConsoleApplication/Matura2022MajConsoleApplication.cpp
+++ Programowanie/Matura2022MajConsoleApplication/Matura2022MajConsoleApplication.cpp
@@ -2,9 +2,47 @@
 
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::ifstream file("przyklad.txt");
+	/*
+
+    int numbers[200];
+
+	for (int i = 0; i < 200; i++)
+	{
+		file >> numbers[i];
+	}
+
+	std::cout << "Odczytanie liczby \n";
+	for (int i = 0; i < 200; i++)
+	{
+		std::cout << numbers[i] << ", ";
+
+	}
+	std::cout << "\n";
+	*/
+
+	std::vector<int> numbers;
+
+	int num;
+	while (file >> num)
+		numbers.push_back(num);
+	/*
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		std::cout << numbers[i] << ", ";
+	}
+	*/
+
+	std::cout << "Odczytane liczby: \n";
+	for (int num : numbers) 
+	{
+		std::cout << num << ", ";
+	}
+	std::cout << "\n";
 }
 
