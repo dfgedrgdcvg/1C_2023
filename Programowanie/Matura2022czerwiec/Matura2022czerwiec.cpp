c57a74bd70f6fc355c3c56b86987f7491f622aed
O <O>
2025-04-09 13:16:42 +0200

    zadanie 4.1 i 4.2

--- Programowanie/Matura2022czerwiec/Matura2022czerwiec.cpp
+++ Programowanie/Matura2022czerwiec/Matura2022czerwiec.cpp
@@ -1,35 +1,60 @@
-// Matura2022czerwiec.cpp 
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <set>
 
+
+void zadanie4_1()
+{
+	std::ifstream file("przyklad.txt");
+	std::vector<int> numbers;
+	int number;
+
+	std::cout << "Zadanie 4.1 \n";
+	while (file >> number) {
+		int reserved = 0;
+		int temp = number;
+		while (temp > 0) {
+			int digit = temp % 10;
+			reserved = reserved * 10 + digit;
+			temp /= 10;
+		}
+		if (reserved % 17 == 0) {
+			std::cout << reserved << ", " << "\n";
+		}
+	}
+}
+
+void zadanie4_2() {
+	std::ifstream file("przyklad.txt");
+	std::vector<int> numbers;
+
+	int number;
+	int maxDifference = 0;
+	int numberWithMaxDifference = 0;
+	std::cout << "Zadanie 4.2 \n";
+
+	while (file >> number) {
+		int reversed = 0;
+		int temp = number;
+
+		while (temp > 0) {
+			int digit = temp % 10;
+			reversed = reversed * 10 + digit;
+			temp /= 10;
+		}
+		int difference = number - reversed;
+		if (difference < 0) {
+			difference = -difference;
+		}
+		if (difference > maxDifference) {
+			maxDifference = difference;
+			numberWithMaxDifference = number;
+		}
+	}
+	std::cout << numberWithMaxDifference << ", " << maxDifference << "\n";
+}
 int main()
 {
-    std::ifstream file("przyklad.txt");
-
-
-    std::cout << "Zadanie 4.1 \n";
-    int number;
-    while (file >> number) {
-        int reserved = 0;
-        int temp = number;
-        while (temp > 0) {
-            int digit = temp % 10;
-            reserved = reserved * 10 + digit;
-            temp /= 10;
-        }
-        if (reserved % 17 == 0)
-        {
-            std::cout << reserved << ", " << "\n";
-        }
-    }
-    std::cout << "Zadanie 4.2";
-    int number = 0;
-    int maxDiffrence = 0;
-    int numberWithMaxDiffrence = 0;
-    while (file >> number) {
-        int reversed = 0;
-        int temp = number;
-    }
+	zadanie4_1();
+	zadanie4_2();
 }
