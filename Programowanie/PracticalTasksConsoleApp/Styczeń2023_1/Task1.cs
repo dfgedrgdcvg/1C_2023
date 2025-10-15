9e2fdb1ae8c526dea1aaf71b9775db2413a6bd3e
Sala213 <Sala213@ezse.pl>
2025-10-15 15:58:08 +0200

    robimy egzaminu

---
+++ Programowanie/PracticalTasksConsoleApp/Styczeń2023_1/Task1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalTasksConsoleApp.Styczeń2023_1
+{
+    internal class Task1
+    {
+        public void Task()
+        {
+            Console.WriteLine("Podaj pierwsza liczbę całkowitą: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj drugą liczbę całkowitą: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Najwiekszy wpolny dzielnik (NWD) to: "
+        }
+    }
+}
